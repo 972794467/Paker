6c5937ce0e9b9ac24dc42c549ebcda9b7de31904
xwhdlm <972794467@qq.com>
2020-09-06 21:44:40 +0800

    modify
    modify pake

--- paker/js/lang_lib.js
+++ paker/js/lang_lib.js
@@ -1,4 +1,4 @@
-var lang_company_name = ["Hebei Paker Rubber Products Co., Ltd.","河北帕克橡塑制品有限公司"]
+var lang_company_name = ["Hebei Pake Rubber Products Co., Ltd.","河北帕克橡塑制品有限公司"]
 
 var lang_title = ["Home","首页   "];
 var lang_about = ["About","关于我们"];
@@ -9,12 +9,12 @@ var lang_equipment = ["Equipment","工厂设备"];
 var lang_banner1 = ["Adopt the world advanced level, rely on science and technology development<span>Take honesty as the example and environmental protection as the foundation.</span>","采 用 世 界 先 进 水 平，靠 科 技 发 展<span>以 诚 信 为 宗，环 保 为 本</span>"];
 var lang_banner2 = ["Quality is life, continuous improvement, constant innovation.<span>High quality and high starting point is what we should do.</span>","质 量 就 是 生 命，持 续 改 进，不 断 创 新<span>高 品 质 高 起 点 ，是 我 们 每 个 帕 克 人 应 该 做 的</ span>"];
 
-var lang_introduction_title = ["Welcome to Hebei Paker Rubber Products Co., Ltd.","欢迎来到河北帕克橡塑制品有限公司"];
-var lang_introduction = ["Paker is a speciallized manufacturer for hydraulic pipe fittings,with a group of high-level designers and employees and having rich experience in producing high pressure hose,hose couplings,transition joints and quick couplings.","Paker是一家专业生产液压管件的制造商，我司拥有一批高级工程师和优秀职工，在生产高压软管，软管接头，过渡接头和快速接头方面拥有丰富的经验。"];
+var lang_introduction_title = ["Welcome to Hebei Pake Rubber Products Co., Ltd.","欢迎来到河北帕克橡塑制品有限公司"];
+var lang_introduction = ["Pake is a speciallized manufacturer for hydraulic pipe fittings,with a group of high-level designers and employees and having rich experience in producing high pressure hose,hose couplings,transition joints and quick couplings.","Pake是一家专业生产液压管件的制造商，我司拥有一批高级工程师和优秀职工，在生产高压软管，软管接头，过渡接头和快速接头方面拥有丰富的经验。"];
 var lang_quality_introduction_title = ["Our Quality Products ","高 质 量 产 品"];
 var lang_quality_introduction = ["With perfect quality management system and advanced design and superior structure,Products the factory producing are popular all over the world with high quality and reasonable price,Above all,the factory has passed the authentication of ISO9001:2008 Quality System.We hope to cooperate with all old and new customers.","凭借完善的质量管理体系，先进的设计和优越的结构，我司生产的产品畅销世界各地，质量上乘，价格合理。工厂产品已通过ISO9001：2008质量体系认证。我们希望所有新老客户精诚合作，共创未来。"];
 
-var lang_about_us_1 = ["Paker is an export-oriented enterprise that mainly develops, produces and sells Eaton and Paker standard products. The company specializes in producing Eaton and Paker standard hose joints. Hose assembly, sandblasting tube, oxygen lance, drilling tube, hydraulic cylinder, metal hose and various rubber nylon products. The product has the characteristics of high pressure, small bending radius, strong fatigue resistance and long service life. It is widely used in metallurgy, petroleum, engineering machinery, mining and various types of hydraulic machinery.","Paker公司是一家集研发、生产和销售伊顿及派克标准产品为主的出口型企业，本公司专业生产伊顿及派克标准软管接头。胶管总成、喷砂管、吹氧管、钻探管、液压缸、金属软管及各种橡胶尼龙制品。产品具有承受压力高、弯曲半径小、抗疲劳性能强、使用寿命长等特点，广泛应用于冶金、石油、工程机械、矿山采掘及各类液压机械。 "];
+var lang_about_us_1 = ["Pake is an export-oriented enterprise that mainly develops, produces and sells Eaton and Pake standard products. The company specializes in producing Eaton and Pake standard hose joints. Hose assembly, sandblasting tube, oxygen lance, drilling tube, hydraulic cylinder, metal hose and various rubber nylon products. The product has the characteristics of high pressure, small bending radius, strong fatigue resistance and long service life. It is widely used in metallurgy, petroleum, engineering machinery, mining and various types of hydraulic machinery.","Pake公司是一家集研发、生产和销售伊顿及派克标准产品为主的出口型企业，本公司专业生产伊顿及派克标准软管接头。胶管总成、喷砂管、吹氧管、钻探管、液压缸、金属软管及各种橡胶尼龙制品。产品具有承受压力高、弯曲半径小、抗疲劳性能强、使用寿命长等特点，广泛应用于冶金、石油、工程机械、矿山采掘及各类液压机械。 "];
 var lang_about_us_2 = ["In recent years, with the continuous expansion of business, the company's products sell well throughout the country and are exported to Russia, Germany, Iran, Pakistan, Europe, America, Southeast Asia and other countries, and are trusted by domestic and foreign users.","近年来，随着业务的不断扩大，本公司产品畅销全国，并出口，俄罗斯、德国、伊朗，巴基斯坦，欧美东南亚等国家，深受国内外用户的信赖。"];
 var lang_about_us_3 = ["The company is based on integrity, innovation, and environmental protection. We are willing to work together with friends from all walks of life to meet your needs with excellent products and sincere service. We warmly welcome new and old customers at home and abroad to come and visit us and negotiate business!","本公司以诚信，创新，环保， 为宗旨。愿与各界朋友携手共进，以卓越的产品、诚信的服务满足您的需求，热忱欢迎国内外新老客户前来光临指导，洽谈业务！"];
 
@@ -36,7 +36,7 @@ var lang_pro_class6 = ["Tetrafluorocarbon products","四氟制品"];
 var lang_pro_class7 = ["Polyurethane products","聚氨酯制品"];
 var lang_pro_class8 = ["Metal hose","金属软管"];
 
-var lang_contant_company_name = ["Hebei Paker Rubber Products Co., Ltd.","河 北 帕 克 橡 塑 制 品 有 限 公 司"];
+var lang_contant_company_name = ["Hebei Pake Rubber Products Co., Ltd.","河 北 帕 克 橡 塑 制 品 有 限 公 司"];
 var lang_contant_telephone = ["Telephone: 0318-4239333","电&emsp;话&nbsp;： 0318-4239333"];
 var lang_contant_phone = ["Phone number : 13931835123","手&emsp;机&nbsp;： 13931835123 &nbsp;"];
 var lang_contant_phone2 = ["&emsp;&emsp;&nbsp;&emsp;&emsp;&nbsp;&emsp;&emsp;&emsp;&nbsp;15100387566","&emsp;&emsp;&nbsp;&emsp;&emsp;&nbsp;15100387566"];	
